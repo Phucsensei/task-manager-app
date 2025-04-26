b814d4b97d84ad0787f54463fa74bb8be7e8cb81
phuc <phuct6776@gmail.com>
2025-04-26 16:49:02 +0700

    deployment

--- task-manager-app/src/App.tsx
+++ task-manager-app/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
-import TaskProvider from './feature/context/taskProvider';
 import { ThemeProvider } from './feature/context/ThemeContext';
+import TaskProvider from './feature/context/taskProvider';
 
 function App() {
   return (
